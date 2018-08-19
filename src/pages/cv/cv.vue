fb72346654ec3a4b858ae12e21044b0a9dbf19d9
binnull <binnull@foxmail.com>
2018-08-19 16:06:49 +0800

    页面完成

---
+++ src/pages/cv/cv.vue
@@ -0,0 +1,75 @@
+<template>
+  <section class="cvpage">
+    <div class="cvpage-left">
+      <section class="cvpage-left-wrap">
+        <section class="cvpage-left-top">
+          <div class="cvpage-left-pinfo">
+            <h2 class="cvpage-left-pinfo--name">胡兵兵</h2>
+            <p class="cvpage-left-pinfo--title">UX&UI Designer</p>
+            <div class="cvpage-left-pinfo--school-wrap">
+              <p class="cvpage-left-pinfo--school">本科 · 中央美术学院 /</p>
+              <p class="cvpage-left-pinfo--school">设计学院 / 数码媒体</p>
+            </div>
+            <div class="cvpage-left-pinfo--phone-wrap">
+              <p class="cvpage-left-pinfo--phone">+ 86 186 1385 9561</p>
+              <p class="cvpage-left-pinfo--email">bingo@holyhilab.com</p>
+            </div>
+          </div>
+          <div class="cvpage-left-pimg">
+            <img src="./image/pimg.jpg">
+          </div>
+        </section>
+        <hr class="cvpage-left-hr" />
+        <section class="cvpage-left-bottom">
+          <section class="cvpage-left-block">
+            <div class="cvpage-left-block--title">个人简介</div>
+            <div class="cvpage-left-block--content">
+              发现问题、提出问题、解决问题和创造新的视觉语言是我热爱的工作，不断学习，总结设计思维和方法。倡导恰当的用户体验，结构化的设计和极其细致的执行，以及全局，动态和非线性协同创新的设计思维。重视设计思维和设计方法;独立自律的习惯，积极解决问题的态度;勇于承担不推卸责任，善于沟通乐于创新。
+            </div>
+          </section>
+          <section class="cvpage-left-block">
+            <div class="cvpage-left-block--title">个人简历</div>
+            <div class="cvpage-left-block--content">
+              <p>2012前生活并就读于内蒙古包头市;</p>
+              <p>2012.09就读于中央美术学院;</p>
+              <p>2014.01-05实习于西门子（北京）中国研究院| 视频设计实习生，负责设计思考部门讲习班视频拍摄及后期制作，参与Workshop讨论;</p>
+              <p>2014.07-09实习于Dexter China | 原画设计实习生，参与制作“智取威虎山”，“三少爷的剑”电影的原画设计;</p>
+              <p>2014.09进入中央美术学院数码媒体·交互设计专业;</p>
+              <p>2015.07-2016.06毕业设计作品“向素 - 零设计，零代码自助建站工具”，旨在通过简洁的交互方式为创意工作者提供自助建站工具，毕业展览期间上线运营俩个月; </p>
+              <p>2016.06毕业于中央美术学院;</p>
+              <p>2016.07-09工作于青云| 交互设计师，负责AppCenter交互设计，产品设计，及青云QingCloud控制台组件化设计;</p>
+              <p>2016.10-至今创立HolyhiLab互动媒体设计实验室| 交互/视觉设计师，致力于设计思维、方法和创新视觉语言探索和实践。在此期间曾负责千万级用户产品的交互和视觉设计工作。</p>
+              <p>2017.11-至今工作于懒投资| UED·交互设计师，负责基因检测和肠道微生物检测等相关产品的交互设计工作。</p>
+            </div>
+          </section>
+          <section class="cvpage-left-block">
+            <div class="cvpage-left-block--title">个人简介</div>
+            <div class="cvpage-left-block--content">
+              <p>2013年春季社会实践系列作品中央美术学院校史馆展出</p>
+              <p>2014年获得国家励志奖学金</p>
+              <p>2014年“一亩三分田”项目获得北京国际设计周创客马拉松二等奖</p>
+              <p>2015年“一亩三分田”项目获得北京大学生创新·创业大赛一等奖</p>
+              <p>2015年移动互信媒体设计作品“理想国”入围微软创新杯全球比赛，并获得中央美术学院优秀课程团队作品优秀奖。</p>
+              <p>2016年“向素”获得北京市大学生创业大赛二等奖，中央美术学院第六届创业大赛最具表现力奖。</p>
+              <p>2017年多个项目入围站酷榜单</p>
+            </div>
+          </section>
+          <section class="cvpage-left-block">
+            <div class="cvpage-left-block--title">个人简介</div>
+            <div class="cvpage-left-block--content">
+              <p>2017年年1月中央美术学院城市设计学院|“UX和UI设计中的系统化思维”主要分享UX和UI设计过程中系统性思维的重要性，以及如何使设计组件化和组件化带来的影响。</p>
+              <p>2018年4月13日中华女子学院|。“商业驱动的交互设计”新的商业环境对设计师提出了更高的要求，在这样快速变化的背景下，以我个人为例，在这个过程中做过的，在做的一些事情，并着重分享我在应对这一变化过程中所学习并正在使用的一些设计思维和方法。</p>
+            </div>
+          </section>
+        </section>
+      </section>
+    </div>
+    <div class="cvpage-right"></div>
+  </section>
+</template>
+<script>
+export default {}
+</script>
+<style lang="less">
+@import url('./cv.less');
+</style>
